330d7e4e11c2cb6ac3bb45ef6d76c52f8f27e8b0
Christopher Rodriguez <cdr255@gmail.com>
2012-03-12 12:38:25 -0400

    First Commit. README, DEPENDENCIES, and SURGE.h created.

---
+++ SURGE.h
@@ -0,0 +1,11 @@
+// This is SURGE - the Simple Universal Realistic Game Engine. It does
+// NOT attempt to me a hold-your-hand, do-everything-for-you engine
+// like... every other engine I have ever used. All this does is
+// compile the common methods I use in every game I make, so I can
+// stop rewriting the same code over and over and over. YMMV, HTH.
+#ifndef _SURGE_H_
+#define _SURGE_H_
+
+#include <SDL/SDL.h>
+
+#endif
